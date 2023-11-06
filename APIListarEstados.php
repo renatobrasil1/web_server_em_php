1cfbbcc9a0107ba87188840a5a3ede962fae5df6
renatobrasil1 <nato-brasil@hotmail.com>
2023-11-06 13:59:05 -0300

    implementado AsynTask da sessão 3 Aula 29

--- APIListarEstados.php
+++ APIListarEstados.php
@@ -1,40 +1,56 @@
 <?php
 
-header('Content-type: application/json');
+if ($_SERVER['REQUEST_METHOD'] == 'POST') { 
+    
+     header('Content-type: application/json');
+    // TOKEN
+    
+    
+    $api_token = "fabricadedesenvolvedores";
+    
+    if($api_token == "fabricadedesenvolvedores"){
+        
+        $response['auth_token'] = false;
+        
+        echo json_encode($response);
+    }
+    
+   
 
 // Conexão com Banco de Dados
-require_once 'dbConnect.php';
+    require_once 'dbConnect.php';
 
 // Definir UTF8 para a conexão
 
-mysqli_set_charset($conn, $charset);
+    mysqli_set_charset($conn, $charset);
 
-$response = array();
+    $response = array();
 
-$stmt = mysqli_prepare($conn, "SELECT id, sigla, nome FROM estado");
+    $stmt = mysqli_prepare($conn, "SELECT id, sigla, nome FROM estado");
 
 
-mysqli_stmt_execute($stmt);
-mysqli_stmt_store_result($stmt);
-mysqli_stmt_bind_result($stmt,
-        $id,
-        $sigla,
-        $nome);
+    mysqli_stmt_execute($stmt);
+    mysqli_stmt_store_result($stmt);
+    mysqli_stmt_bind_result($stmt,
+            $id,
+            $sigla,
+            $nome);
 
 // Apresentando os Dados da consulta
 //var_dump($stmt);
 
 
-if (mysqli_stmt_num_rows($stmt) > 0) {
+    if (mysqli_stmt_num_rows($stmt) > 0) {
 
-    while (mysqli_stmt_fetch($stmt)) {
-        array_push($response, array("id" => $id,
-            "sigla" => $sigla,
-            "nome" => $nome));
+        while (mysqli_stmt_fetch($stmt)) {
+            array_push($response, array("id" => $id,
+                "sigla" => $sigla,
+                "nome" => $nome));
+        }
+        echo json_encode($response);
+    } else {
+
+        echo json_encode($response);
     }
-    echo json_encode($response);
-} else {
-
-    echo json_encode($response);
 }
 ?>
